2cf4ffef16c52a6c568586513256f8faa4b322ec
Matthew Garner <1matthewgarner@gmail.com>
2021-05-02 21:58:11 +0100

    fix image not working remotely v3

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -1,8 +1,6 @@
 function createHeroArea () {
     const heroArea = document.createElement('div');
     heroArea.id = 'hero-area';
-    heroArea.style.backgroundImage = "url('/imgs/hero-image.jpg')";
-    heroArea.style.backgroundSize = "100% 100%";
 
     //Add hero text
     const heroText = document.createElement('h2');
--- src/modules/home.js
+++ src/modules/home.js
@@ -106,9 +106,7 @@ function createMain() {
 function createHeroArea () {
     const heroArea = document.createElement('div');
     heroArea.id = 'hero-area';
-    heroArea.style.backgroundImage = "url('/imgs/hero-image.jpg')";
-    heroArea.style.backgroundSize = "100% 100%";
-
+   
     //Add hero text
     const heroText = document.createElement('h2');
     heroText.textContent = 'The biggest, tastiest pies in all Yorkshire';
